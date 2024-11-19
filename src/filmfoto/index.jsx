99a79616edcd8844846813b301e9a60cb7daad97
Sheila Kelley <sheilaphedrakelley@gmail.com>
2024-11-18 17:21:44 -0800

    adding images to art section

---
+++ src/filmfoto/index.jsx
@@ -0,0 +1,5 @@
+export { default as BlurryL } from './blurry_L.jpeg';
+export { default as BlurrySage } from './blurry_sage.jpeg'
+// export { default as First } from './photos/first.jpg';
+// export { default as Second } from './photos/second.jpg';
+// export { default as LinkedIn } from './social/linkedin.png';
--- src/pages/Art.jsx
+++ src/pages/Art.jsx
@@ -1,11 +1,13 @@
+import React from 'react';
 import '../App.scss';
 import '../Art.scss';
+import { BlurryL, BlurrySage } from '../filmfoto';
 
-// const images = require.context('../filmfoto', true);
-
-
-const images = import('../filmfoto')
+const images = [BlurryL, BlurrySage]
+console.log(images)
+// let images = import({ BlurryL from filmfoto' } );
 const imageList = images.keys().map(image => images(image));
+
 
 // const s3Uri = 's3://arn:aws:s3:us-west-2:095758704663:accesspoint/foto-access'
 
