195c38fd6b015b0911db9104e204e7032a0e8c3a
Sheila Kelley <sheilaphedrakelley@gmail.com>
2025-03-10 15:57:50 -0700

    cards complete :)

--- src/components/FlipCardContainer.jsx
+++ src/components/FlipCardContainer.jsx
@@ -6,25 +6,25 @@ const projects = [
     {
       title: 'This Portfolio',
       imageUrl: '/assets/portfolio.png',
-      description: "What you're looking at right now. A portfolio site for software and photography, showcasing my experience, design, and ability. This site was made with React, Vite, and SCSS, and is hosted on Vercel.",
+      description: "What you're looking at right now, a portfolio site for my software projects and photography. This site was made with React, Vite, and SCSS, and is hosted on Vercel.",
       links: ['https://sheila-kelley.com/', 'https://github.com/shephekay/2024-portfolio']
     },
     {
         title: "Sheila's Best of Portland",
         imageUrl: '/assets/places.png',
-        description: 'placeholder placeholder placehoplder nfdoiuefnjwjr cvnijudcjnuiodsv vdsmndsjivdsojikd svdkmdskmd',
+        description: 'This full-stack application is a collection of my favorite places in Portland, and lets users add and edit their own. Built with React, Node.js, MongoDB, Mongoose, Express, and Vanilla CSS, it has full CRUD functioning, and adheres to RESTful architecture and the MVC file structure. Hosted with Render - allow a few seconds of latency as Render spins down the site with inactivity.',
         links: ['https://places-app-2025.onrender.com/', 'https://github.com/shephekay/places-app-2025']
     },
     {
         title: 'Complaints to NYPD',
         imageUrl: '/assets/complaints.png',
-        description: 'placeholder blahhhhh',
+        description: "Using the NYPD Complaint Data API from NYC Opendata, this project shows the most recent complaints to NYPD from the selected borough, and the police's response. Made with HTML, CSS, Javascript, and jQuery in 2020.",
         links: ['https://shephe.github.io/complaints/', 'https://github.com/shephekay/complaints-reboot']
     },
     {
         title: 'Mushroom Hunt',
         imageUrl: '/assets/mushroom.png',
-        description: 'A simple browser game',
+        description: 'A simple browser game made with just HTML, CSS, Javascript, and jQuery, also from 2020. One of the very first coding projects I ever made! Two players alternate turns clicking the board trying to gather as many mushrooms as they can while avoiding the Death Cap.',
         links: ['https://shephe.github.io/mushroom-hunt/', 'https://github.com/shephekay/mushroom-hunt']
     },
 
